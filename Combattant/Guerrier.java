8430fffdcedd219edc95284b3882ab921f85f8f7
kairo00 <h.marion39@gmail.com>
2025-04-17 14:03:25 +0200

    delete folder 'Combattant'

--- Combattant/Guerrier.java
+++
@@ -1,5 +0,0 @@
-public class Guerrier extends Combattant{
-    public Guerrier() {
-        super(220, 30, 25, 20);
-    }
-}
--- Combattant/Mage.java
+++
@@ -1,5 +0,0 @@
-public class Mage extends Combattant {
-    public Mage() {
-        super(160, 70, 0, 30);
-    }
-}
